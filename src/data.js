e75c2b4cc52503ed75dce2113f0249241d64bc6f
sakkarin1202 <xfake12340@hotmail.com>
2023-09-29 13:26:23 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        id:1,
+        name: "Jonas Kakaroto",
+        age: 36,
+        image:"https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+    },
+    {
+        id: 2,
+        name: "Kelly Sikkema",
+        age: 26,
+        image: "https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1476&q=80"
+    },
+    {
+        id: 3,
+        name: "than Hoover",
+        age: 24,
+        image: "https://images.unsplash.com/photo-1500048993953-d23a436266cf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80"
+    },
+    {
+        id: 4,
+        name: "Foto Sushi",
+        age: 49,
+        image: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+    },
+    {
+        id: 5,
+        name: "Moemimk Sung",
+        age: 23,
+        image: "https://images.unsplash.com/photo-1525875975471-999f65706a10?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+    }
+];
+
+export default data;
